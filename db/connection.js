d06404a7ed75308729e7cd6c9a63e6f7b9d4a251
Taomosquito <S_Peloquin123@live.com>
2024-08-16 00:08:11 -0400

    update connection to have access to both database

--- db/connection.js
+++ db/connection.js
@@ -9,8 +9,20 @@ const dbParams = {
   database: process.env.DB_NAME
 };
 
+const dbEventsParams = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_SECONDARY
+};
+
+const dbEvents = new Pool(dbEventsParams);
+
 const db = new Pool(dbParams);
 
+dbEvents.connect();
 db.connect();
 
+module.exports = dbEvents;
 module.exports = db;
