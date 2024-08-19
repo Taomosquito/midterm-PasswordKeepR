6bc4d40726cd84ca7360fede8b8c1e8496d3c31b
Daman Bual <damanbual@gmail.com>
2024-08-19 07:57:19 -0700

    added privacy, contact, tos pages and ejs files

--- routes/contact.js
+++ routes/contact.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Contact');
+  res.render('contact');
 });
 
 module.exports = router;
--- routes/privacyPolicy.js
+++ routes/privacyPolicy.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Privacy Policy');
+  res.render('privacyPolicy');
 });
 
 module.exports = router;
