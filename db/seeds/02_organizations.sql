53ed5f88ecf03c6fc9ef3c1642b1b97355a0c152
Taomosquito <S_Peloquin123@live.com>
2024-08-20 00:50:39 -0400

    add seed file to organizations table

---
+++ db/seeds/02_organizations.sql
@@ -0,0 +1,4 @@
+-- Widgets table seeds here (Example)
+INSERT INTO organizations (organization) VALUES ('Red Hat');
+INSERT INTO organizations (organization) VALUES ('Microsoft');
+INSERT INTO organizations (organization) VALUES ('Apple');
